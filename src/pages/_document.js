b4143b01bbf291cc34d36cea2e47809a93699bd7
sarathdixit <sarathdixit29@gmail.com>
2023-11-19 12:49:09 +0530

    source added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="3zySWWgJ1VrvYWpTGOLpYl9gz_TQrkI0t0-vS-FoezM"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
