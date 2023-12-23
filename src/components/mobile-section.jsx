dfd4dbabf0abba131d44c3174b4bca34e40c6bf0
sarathdixit <sarathdixit29@gmail.com>
2023-12-23 19:44:39 +0530

    done

--- src/components/mobile-section.jsx
+++ src/components/mobile-section.jsx
@@ -5,7 +5,9 @@ export default function MobileSection({ heading, description, hide = false }) {
     <div className="!px-5">
       <div className="flex gap-3 items-start">
         <div className="w-[1px] h-[20px] bg-black" />
-        <div className="!font-neue text-[24px] leading-[24px]">{heading}</div>
+        <div className="!font-neue font-[500] text-[24px] leading-[24px]">
+          {heading}
+        </div>
       </div>
       {!hide && (
         <p className="!font-neue font-[500] tracking-[1.2px] text-[30px] max-w-[80%] m-auto !mt-10 text-center leading-[34.5px] spacing-[1.2px]">
